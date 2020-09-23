a784b302dfc952bf692e77bf1f2c16e784f0b390
azibom <azibom@azibom.com>
2020-09-23 21:03:30 +0330

    Fix the api.php style

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,16 +1,33 @@
 <?php
+/**
+ * api
+ *
+ * PHP version 7
+ *
+ * @category Route
+ * @package  Route
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->group(function () {
-    Route::group(['namespace' => 'Azibom\whoAreYou\Controllers'], function(){
-        Route::post('login', 'UserController@login');
-        Route::post('register', 'UserController@register');
-        Route::post('refreshtoken', 'UserController@refreshToken');
-        
-        Route::group(['middleware' => ['auth:api']], function () {
-            Route::post('logout', 'UserController@logout');
-            Route::post('details', 'UserController@details');
-        });
-    });
-});
+Route::prefix('api')->group(
+    function () {
+        Route::group(
+            ['namespace' => 'Azibom\whoAreYou\Controllers'],
+            function () {
+                    Route::post('login', 'UserController@login');
+                    Route::post('register', 'UserController@register');
+                    Route::post('refreshtoken', 'UserController@refreshToken');
+                    Route::group(
+                        ['middleware' => ['auth:api']], function () {
+                            Route::post('logout', 'UserController@logout');
+                            Route::post('details', 'UserController@details');
+                        }
+                    );
+            }
+        );
+    }
+);
