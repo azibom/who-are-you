5a42cf6f3fce579b084c3d43f10c69ecb457de8b
azibom <azibom@azibom.com>
2020-09-23 21:06:25 +0330

    Fix WhoAreYouServiceProvider.php code style

--- src/whoAreYouServiceProvider.php
+++ src/whoAreYouServiceProvider.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * whoAreYouServiceProvider
+ *
+ * PHP version 7
+ *
+ * @category ServiceProvider
+ * @package  ServiceProvider
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 
 namespace Azibom\whoAreYou;
 
@@ -7,6 +18,15 @@ use Azibom\whoAreYou\Repositories\User\UserRepository;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
 
+/**
+ * WhoAreYouServiceProvider Class
+ *
+ * @category ServiceProvider
+ * @package  ServiceProvider
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 class WhoAreYouServiceProvider extends ServiceProvider
 {
     /**
@@ -14,8 +34,7 @@ class WhoAreYouServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
     }
 
@@ -24,8 +43,7 @@ class WhoAreYouServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         Passport::routes();
         $this->loadRoutesFrom(__DIR__.'/routes/api.php');
     }
