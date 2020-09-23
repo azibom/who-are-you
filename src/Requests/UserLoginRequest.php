3d7389c616071304fb03c7bd3e5b0ad198f68c7f
azibom <azibom@azibom.com>
2020-09-23 20:26:28 +0330

    Fix the style of UserLoginRequest.php

--- src/Requests/UserLoginRequest.php
+++ src/Requests/UserLoginRequest.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * UserLoginRequest
+ *
+ * PHP version 7
+ *
+ * @category Request
+ * @package  Request
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 
 namespace Azibom\whoAreYou\Requests;
 
@@ -6,9 +17,24 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class UserLoginRequest extends FormRequest {
+/**
+ * UserLoginRequest Class
+ *
+ * @category Request
+ * @package  Request
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+class UserLoginRequest extends FormRequest
+{
     const UNPROCESSABLE_ENTITY = 422;
 
+    /**
+     * rules function
+     *
+     * @return array
+     */
     public function rules() {
         return [
             'email' => 'required|email',
@@ -16,7 +42,18 @@ class UserLoginRequest extends FormRequest {
           ];
     }
 
+    /**
+     * failedValidation function
+     *
+     * @param Validator $validator That is a validator
+     * @return void
+     */
     protected function failedValidation(Validator $validator) {
-        throw new HttpResponseException(response()->json($validator->errors(), self::UNPROCESSABLE_ENTITY));
+        throw new HttpResponseException(
+            response()->json(
+                $validator->errors(),
+                self::UNPROCESSABLE_ENTITY
+            )
+        );
     }
 }
