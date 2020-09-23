398c0737473c88c98fc5593ddbd13cbdd3f56f18
azibom <azibom@azibom.com>
2020-09-23 12:32:40 +0330

    add the phpcs to the project

--- src/Requests/UserRegisterRequest.php
+++ src/Requests/UserRegisterRequest.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Test
+ *
+ * PHP version 7
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 
 namespace Azibom\whoAreYou\Requests;
 
@@ -6,9 +17,26 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class UserRegisterRequest extends FormRequest {
+/**
+ * Template Class Doc Comment
+ *
+ * Template Class
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+class UserRegisterRequest extends FormRequest
+{
     const UNPROCESSABLE_ENTITY = 422;
 
+    /**
+     * rules function
+     *
+     * @return array
+     */
     public function rules() {
         return [
             'name' => 'required',
@@ -18,7 +46,18 @@ class UserRegisterRequest extends FormRequest {
           ];
     }
 
+    /**
+     * failedValidation function
+     *
+     * @param Validator $validator that is validator
+     * @return void
+     */
     protected function failedValidation(Validator $validator) {
-        throw new HttpResponseException(response()->json($validator->errors(), self::UNPROCESSABLE_ENTITY));
+        throw new HttpResponseException(
+            response()->json(
+                $validator->errors(),
+                self::UNPROCESSABLE_ENTITY
+            )
+        );
     }
 }
