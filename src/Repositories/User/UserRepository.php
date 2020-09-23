9a2ea3bb9b9897f37b98379285213c15e728ce2d
azibom <azibom@azibom.com>
2020-09-23 22:30:20 +0330

    Fix the style of UserRepository

--- src/Repositories/User/UserRepository.php
+++ src/Repositories/User/UserRepository.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * UserRepository
+ *
+ * PHP version 7
+ *
+ * @category Repository
+ * @package  Repository
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 
 namespace Azibom\whoAreYou\Repositories\User;
 
@@ -7,105 +18,189 @@ use Azibom\whoAreYou\Contracts\User\UserRepositoryInterface;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Passport\Client as OClient;
+use Laravel\Passport\Client as Oclient;
 use GuzzleHttp\Exception\ClientException;
 
-
-class UserRepository implements UserRepositoryInterface {
-    const SUCCUSUS_STATUS_CODE = 200;
+/**
+ * UserRepository Class
+ *
+ * @category Repository
+ * @package  Repository
+ * @author   Azibom <mrsh13610@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+class UserRepository implements UserRepositoryInterface
+{
+    const SUCCUSUS_STATUS_CODE     = 200;
     const UNAUTHORISED_STATUS_CODE = 401;
 
+    /**
+     * __construct function
+     *
+     * @param Client $client client var
+     */
     public function __construct(Client $client) {
-        $this->http = $client;
+        $this->http    = $client;
         $this->baseUrl = env('WHO_ARE_YOU_BASE_URL');
     }
 
+    /**
+     * register function
+     *
+     * @param Request $request request var
+     * @return mix
+     */
     public function register(Request $request) {
-        $email = $request->email;
-        $password = $request->password;
-        $input = $request->all();
+        $email             = $request->email;
+        $password          = $request->password;
+        $input             = $request->all();
         $input['password'] = bcrypt($input['password']);
         User::create($input);
         $response = $this->getTokenAndRefreshToken($email, $password);
-        return $this->response($response["data"], $response["statusCode"]);
-    }
-
-    public function login(Request $request) {
-        $email = $request->email;
-        $password = $request->password;
-
-        if (Auth::attempt(['email' => $email, 'password' => $password])) {
-            $response = $this->getTokenAndRefreshToken($email, $password);
-            $data = $response["data"];
-            $statusCode =  $response["statusCode"];
-        } else {
-            $data = ['error'=>'Unauthorised'];
-            $statusCode =  self::UNAUTHORISED_STATUS_CODE;
-        }
-
-        return $this->response($data, $statusCode);
-    }
-
-    public function refreshToken(Request $request) {
-        if (is_null($request->header('Refreshtoken'))) {
-            return $this->response(['error'=>'Unauthorised'], self::UNAUTHORISED_STATUS_CODE);
-        }
-
-        $refresh_token = $request->header('Refreshtoken');
-        $Oclient = $this->getOClient();
-        $formParams = [ 'grant_type' => 'refresh_token',
-                        'refresh_token' => $refresh_token,
-                        'client_id' => $Oclient->id,
-                        'client_secret' => $Oclient->secret,
-                        'scope' => '*'];
-
-        return $this->sendRequest("/oauth/token", $formParams);
-    }
-
-    public function details() {
-        $user = Auth::user();
-        return $this->response($user, self::SUCCUSUS_STATUS_CODE);
-    }
-
-    public function logout(Request $request) {
-        $request->user()->token()->revoke();
-        return $this->response(['message' => 'Successfully logged out'], self::SUCCUSUS_STATUS_CODE);
-    }
-
-    public function response($data, int $statusCode) {
-        $response = ["data"=>$data, "statusCode"=>$statusCode];
+        $response = $this->response($response["data"], $response["statusCode"]);
         return $response;
     }
 
+    /**
+     * login function
+     *
+     * @param Request $request request var
+     * @return mix
+     */
+    public function login(Request $request) {
+        $email    = $request->email;
+        $password = $request->password;
+
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $response   = $this->getTokenAndRefreshToken($email, $password);
+            $data       = $response["data"];
+            $statusCode = $response["statusCode"];
+        } else {
+            $data       = ['error' => 'Unauthorised'];
+            $statusCode = self::UNAUTHORISED_STATUS_CODE;
+        }
+
+        $response = $this->response($data, $statusCode);
+        return $response;
+    }
+
+    /**
+     * refreshToken function
+     *
+     * @param Request $request request var
+     * @return mix
+     */
+    public function refreshToken(Request $request) {
+        if (is_null($request->header('Refreshtoken'))) {
+            $response = $this->response(
+                ['error' => 'Unauthorised'], self::UNAUTHORISED_STATUS_CODE
+            );
+
+            return $response;
+        }
+
+        $refreshToken = $request->header('Refreshtoken');
+        $oClient      = $this->getOclient();
+        $formParams   = [ 'grant_type' => 'refresh_token',
+                          'refresh_token' => $refreshToken,
+                          'client_id' => $oClient->id,
+                          'client_secret' => $oClient->secret,
+                          'scope' => '*'];
+
+        $response = $this->sendRequest("/oauth/token", $formParams);
+        return $response;
+    }
+
+    /**
+     * details function
+     *
+     * @return mix
+     */
+    public function details() {
+        $user     = Auth::user();
+        $response = $this->response($user, self::SUCCUSUS_STATUS_CODE);
+        return $response;
+    }
+
+    /**
+     * logout function
+     *
+     * @param Request $request request var
+     * @return mix
+     */
+    public function logout(Request $request) {
+        $request->user()->token()->revoke();
+        $response = $this->response(
+            ['message' => 'Successfully logged out'], self::SUCCUSUS_STATUS_CODE
+        );
+
+        return $response;
+    }
+
+    /**
+     * response function
+     *
+     * @param [type]  $data       data var
+     * @param integer $statusCode statusCode var
+     * @return array
+     */
+    public function response($data, int $statusCode) {
+        $response = ["data" => $data, "statusCode" => $statusCode];
+        return $response;
+    }
+
+    /**
+     * getTokenAndRefreshToken function
+     *
+     * @param string $email    email var
+     * @param string $password password var
+     * @return mix
+     */
     public function getTokenAndRefreshToken(string $email, string $password) {
-        $Oclient = $this->getOClient();
+        $oClient    = $this->getOclient();
         $formParams = [ 'grant_type' => 'password',
-                        'client_id' => $Oclient->id,
-                        'client_secret' => $Oclient->secret,
+                        'client_id' => $oClient->id,
+                        'client_secret' => $oClient->secret,
                         'username' => $email,
                         'password' => $password,
                         'scope' => '*'];
 
-        return $this->sendRequest("/oauth/token", $formParams);
+        $request = $this->sendRequest("/oauth/token", $formParams);
+        return $request;
     }
 
+    /**
+     * sendRequest function
+     *
+     * @param string $route      route var
+     * @param array  $formParams formParams var
+     * @return array
+     */
     public function sendRequest(string $route, array $formParams) {
         try {
-            $url = $this->baseUrl.$route;
-            $response = $this->http->request('POST', $url, ['form_params' => $formParams]);
+            $url      = $this->baseUrl.$route;
+            $response = $this->http->request(
+                'POST', $url, ['form_params' => $formParams]
+            );
 
             $statusCode = self::SUCCUSUS_STATUS_CODE;
-            $data = json_decode((string) $response->getBody(), true);
+            $data       = json_decode((string) $response->getBody(), true);
         } catch (ClientException $e) {
-            echo $e->getMessage();
             $statusCode = $e->getCode();
-            $data = ['error'=>'OAuth client error'];
+            $data       = ['error' => 'OAuth client error'];
         }
 
-        return ["data" => $data, "statusCode"=>$statusCode];
+        return ["data" => $data, "statusCode" => $statusCode];
     }
 
-    public function getOClient() {
-        return OClient::where('password_client', 1)->first();
+    /**
+     * getOclient function
+     *
+     * @return Oclient $client
+     */
+    public function getOclient() {
+        $client = Oclient::where('password_client', 1)->first();
+        return $client;
     }
 }
